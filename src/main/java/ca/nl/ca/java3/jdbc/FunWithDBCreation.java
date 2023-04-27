9147cd7f300c765dbeca9e1cdced0f06799dbe70
NicholasOquinn <Nicholas.OQuinn08@ed.cna.nl.ca>
2023-04-27 13:43:45 -0230

    Initial commit

---
+++ src/main/java/ca/nl/ca/java3/jdbc/FunWithDBCreation.java
@@ -0,0 +1,43 @@
+package ca.nl.ca.java3.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * Playing around with Database Creation. This example is from the Unit 2 notes.
+ *
+ * @author Josh
+ */
+public class FunWithDBCreation {
+
+    //Database parameters - "jdbc:mariadb" lets JDBC know to look for the Maria DB driver
+    //TODO Make sure you port and password are set to you own values
+    private static final String DB_URL = "jdbc:mariadb://localhost:3306/";
+    private static final String DB_USER = "root";
+    private static final String DB_PASSWORD = "root";
+
+    public static void main(String[] args) {
+
+        try {
+            //Check DBConfiguration Class if you have any issues with this connection
+            Connection connection = DriverManager.getConnection(DBConfiguration.DB_URL, DBConfiguration.DB_USER, DBConfiguration.DB_PASSWORD);
+            Statement statement = connection.createStatement();
+
+            //TODO Add in a drop table command
+            String sql = "CREATE DATABASE STUDENTS";
+            statement.execute(sql);
+
+            System.out.println("It worked!?");
+
+            statement.close();
+            connection.close();
+        }
+        catch (SQLException sqlException){
+            sqlException.printStackTrace();;
+        }
+
+    }
+
+}
---
+++ src/main/java/ca/nl/ca/java3/jdbc/FunWithDBSelects.java
@@ -0,0 +1,67 @@
+package ca.nl.ca.java3.jdbc;
+
+import java.sql.*;
+
+/**
+ * Playing around with the Employee DB from Unit 2 Notes
+ *
+ * Use the following SQL to configure the database:
+ *
+ * Note: you can use "drop table Employees" to clean the db
+ * <pre>
+ * create table Employees ( Id int not null, age int not null, first varchar (255), last varchar (255) );
+ * INSERT INTO Employees VALUES (100, 40, 'Albert', 'Norman');
+ * INSERT INTO Employees VALUES (101, 42, 'Josh', 'Taylor');
+ * INSERT INTO Employees VALUES (102, 41, 'Richard', 'Spencer');
+ * INSERT INTO Employees VALUES (103, 58, 'Chris', 'Gash');
+ * </pre>
+ *
+ * Note: All of this should be stored in the "employeedb" or change the DB_URL value
+ *
+ * @author Josh
+ */
+public class FunWithDBSelects {
+
+    public static void main(String[] args) {
+
+        try{
+            //Check DBConfiguration Class if you have any issues with this connection
+            Connection connection = DriverManager.getConnection(DBConfiguration.DB_URL + DBConfiguration.DB_EMPLOYEE_DB_NAME, DBConfiguration.DB_USER, DBConfiguration.DB_PASSWORD);
+            printEmployeesTable(connection);
+        }
+        catch (SQLException sqlException){
+            sqlException.printStackTrace();;
+        }
+
+    }
+
+    /**
+     * Print out the employees table. This method is abstracted for reuse in other examples
+     * @param connection open db connection
+     */
+    public static void printEmployeesTable(Connection connection){
+
+        try{
+            Statement statement = connection.createStatement();
+            String sql = "SELECT id, first, last, age FROM employees";
+            ResultSet resultSet = statement.executeQuery(sql);
+
+            System.out.println("Employee data:");
+            System.out.println("ID \t\tAGE \t\tFirst Name\t\tLast Name");
+
+            while(resultSet.next()){
+                // Retrieve by column name
+                System.out.print(resultSet.getInt("id") + "\t\t");
+                System.out.print(resultSet.getInt("age") + "\t\t\t");
+                System.out.print(resultSet.getString("first") + "\t\t\t");
+                System.out.println(resultSet.getString("last") + "\t\t");
+            }
+            statement.close();
+        }
+        catch (SQLException sqlException){
+            sqlException.printStackTrace();;
+        }
+    }
+
+
+}
